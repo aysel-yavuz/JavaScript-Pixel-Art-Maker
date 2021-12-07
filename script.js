81fa6c2567eaf3b23b62a5dcdd273b7ea8819e55
Aysel Yavuz <91015175+aysel-yavuz@users.noreply.github.com>
2021-12-07 13:03:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let grid = document.querySelector('table');
+const button = document.querySelector('[type=submit]');
+button.addEventListener('click', submitClick);
+grid.addEventListener('mousedown', whichMouseButton, true);
+grid.addEventListener('mouseup', stopColoring, true);
+
+function makeGrid() {
+    let height = +document.querySelector('#inputHeight').value;
+    let width = +document.querySelector('#inputWidth').value;  
+    
+    for (let i = 1; i <= height; i++) {
+        let tr = document.createElement('tr');
+        grid.appendChild(tr);
+        for (let j = 1; j <= width; j++) {
+            let td = document.createElement('td');
+            tr.appendChild(td);
+        }
+    }
+}
+
+let firstSubmit = false;
+
+function submitClick(e) {
+    // don't reload page
+    e.preventDefault();
+    // clear existing table
+    if (firstSubmit) {
+        clearTable();
+    }
+    firstSubmit = true;
+    // create new grid
+    makeGrid();
+}
+
+function clearTable() {
+    while (grid.firstChild) {
+        grid.firstChild.remove();
+    }
+}
+
+function startColoring(e) {
+    changeColor(e);
+    grid.addEventListener('mouseover', changeColor);
+}
+
+function changeColor(e) {
+    let color = document.querySelector('#colorPicker').value;
+    if (e.target.nodeName === 'TD') {
+        e.target.style.background = color;
+    };
+}
+
+function stopColoring() {
+    grid.removeEventListener('mouseover', changeColor);    
+}
+
+function erase(e) {
+    if (e.target.nodeName === 'TD') {
+        e.target.style.removeProperty("background-color");
+    };
+}
+
+function whichMouseButton(e) {
+    let mouseButton = e.button;
+    if (mouseButton === 0) {
+        startColoring(e);
+    } else if (mouseButton === 2) {
+        //don't open contenxt menu
+        grid.oncontextmenu = function () {
+            return false;
+        }
+        erase(e);
+    }       
+}
